353bede2f01c69cebe5c85f4f9c85a9f032fab9e
黄旭达 <huangxuda@szkingdom.com>
2025-02-28 09:38:08 +0800

    feat: read config from file

---
+++ config.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Server struct {
+		Ip   string `json:"ip"`
+		Port int    `json:"port"`
+	}
+}
+
+func LoadConfig() (config *Config) {
+	data, err := os.ReadFile("./config.json")
+
+	if err != nil {
+		fmt.Println("Error opening config file:", err)
+		return nil
+	}
+
+	err = json.Unmarshal(data, &config)
+	printServerConfig(config)
+
+	return config
+}
+
+func printServerConfig(config *Config) {
+	fmt.Println("[INFO] Loaded config file.")
+	fmt.Println("[INFO] Server address:", config.Server.Ip)
+	fmt.Println("[INFO] Server port:", config.Server.Port)
+}
