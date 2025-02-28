bffe9d65d520fb53fddd605087e71d42c9fa02e4
黄旭达 <huangxuda@szkingdom.com>
2025-02-28 09:38:16 +0800

    feat: read config from file

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 // Main entrance
 func main() {
 	fmt.Println("---------- ChatOyO Chat Server V0.3 ----------")
-	server := NewServer("127.0.0.1", 2121) // Read from config file
+	config := LoadConfig()
+	server := NewServer(config.Server.Ip, config.Server.Port) // Read from config file
 	server.Run()
 }
